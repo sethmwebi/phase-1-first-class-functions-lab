44bf0c66b9bb1f79fb075dd9fcca31f7a017cafe
Seth Mwebi <sethmwebi27@gmail.com>
2024-06-29 08:26:09 +0300

    writes callback functions that passes for assignment on JavaScript Advanced Functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => {
+  const fareMultiplier = (fare) => {
+    return fare * multiplier;
+  };
+
+  return fareMultiplier;
+};
+
+const fareDoubler = (fare) => {
+  return fare * 2;
+};
+
+const fareTripler = (fare) => {
+  return fare * 3;
+};
+
+const selectDifferentDrivers = (arrayOfDrivers, func) => {
+  return func(arrayOfDrivers);
+};
